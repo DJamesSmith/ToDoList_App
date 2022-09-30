5422b4b8a589348b63dfb1b36c81554a474efb83
Dion James Smith <dion.smith070@gmail.com>
2022-09-30 19:18:45 +0530

    Initial Commit

---
+++ Shared/ToDoListAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListAppApp.swift
+//  Shared
+//
+//  Created by mac on 30/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
